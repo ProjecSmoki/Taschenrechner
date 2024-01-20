cd29a328bba80b9c5c89592d184be68fe3112b0e
MiniSmoki <157181833+MiniSmoki@users.noreply.github.com>
2024-01-20 02:16:03 +0100

    Update Taschenrechner.java

--- Taschenrechner.java
+++ Taschenrechner.java
@@ -7,27 +7,30 @@ public class Taschenrechner {
         Scanner scan = new Scanner(System.in);
         boolean weiter = true;
 
-
         while(weiter) {
             System.out.println("Addieren (1) , Subtrarieren (2) , Multiplikation (3) ,Dividieren (4) ,Programm beenden (5)");
 
             int value = scan.nextInt();
-            double zahl1 = scan.nextDouble();
-            double zahl2 = scan.nextDouble();
 
-            if (value ==1) {
+            if(value != 5) {
+                System.out.println("Tippe die erste Zahl ein; ");
+                double zahl1 = scan.nextDouble();
+                System.out.println("Tippe die zweite Zahl ein: ");
+                double zahl2 = scan.nextDouble();
+            
+            if (value ==1) 
                 plus(zahl1, zahl2);
-            }
-            if (value ==2) {     
+            
+            if (value ==2)     
                 minus(zahl1, zahl2);
-            }
-            if (value ==3) {    
+            
+            if (value ==3)   
                 mal(zahl1, zahl2);
-            }
-            if (value ==4) {    
+            
+            if (value ==4)   
                 durch(zahl1, zahl2);
             }
-            if (value ==5) {
+            else {
                 scan.close();
                 weiter = false;
                 System.out.println("Programm Beendet"); 
@@ -39,14 +42,17 @@ public class Taschenrechner {
 
     public static void plus(double zahl1, double zahl2) { // Methode "+"
     double summe = zahl1 + zahl2;
-        System.out.println( +zahl1 + " + " + zahl2 + " = " + summe); }
+    System.out.println("Aufgabe:" + zahl1 + " + " + zahl2 + " = " + summe); }
+
     public static void minus(double zahl1, double zahl2) { // Methode "-" 
     double summe = zahl1 - zahl2;
-        System.out.println( +zahl1 + " - " + zahl2 + " = " + summe); }
+    System.out.println("Aufgabe:" +zahl1 + " - " + zahl2 + " = " + summe); }
+
     public static void mal(double zahl1, double zahl2) { // Methode "*"
     double summe = zahl1 * zahl2;
-        System.out.println( +zahl1 + " * " + zahl2 + " = " + summe); }
+    System.out.println("Aufgabe:" +zahl1 + " * " + zahl2 + " = " + summe); }
+
     public static void durch(double zahl1, double zahl2) { // Methode "/"
     double summe = zahl1 / zahl2;
-        System.out.println( +zahl1 + " / " + zahl2 + " = " + summe); }
+    System.out.println("Aufgabe:" +zahl1 + " / " + zahl2 + " = " + summe); }
     }
